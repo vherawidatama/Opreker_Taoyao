973c0aa763dc0a78ad3bafcbdafacab4bedb7400
Victory Herawidatama <vherawidatama@gmail.com>
2023-04-09 05:13:28 +0700

    Add files via upload
    Add file new

---
+++ config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Device
+export FOX_BRANCH="fox_9.0"
+export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+
+export DEVICE="beryllium"
+export OEM="xiaomi"
+
+# Build Target
+## "recoveryimage" - for A-Only Devices without using Vendor Boot
+## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
+## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
+export TARGET="recoveryimage"
+
+export OUTPUT="OrangeFox*.zip"
+
+# Additional Dependencies (eg: Kernel Source)
+# Format: "repo dest"
+DEPS=(
+    "https://github.com/OrangeFoxRecovery/Avatar.git misc"
+)
+
+# Extra Command
+export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+
+# Magisk
+## Use the Latest Release of Magisk for the OrangeFox addon
+export OF_USE_LATEST_MAGISK=true
+
+# Not Recommended to Change
+export SYNC_PATH="$HOME/work" # Full (absolute) path.
+export USE_CCACHE=1
+export CCACHE_SIZE="50G"
+export CCACHE_DIR="$HOME/work/.ccache"
+export J_VAL=16
